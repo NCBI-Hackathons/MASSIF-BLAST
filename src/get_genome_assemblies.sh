ce3a7545a456cb91388f1db0d0ffea6aee364309
Chris Ball <christopherball@rti.org>
2018-03-14 14:21:08 -0400

    Update to accept input vars

--- src/get_genome_assemblies.sh
+++ src/get_genome_assemblies.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
 
-wget `esearch -db assembly -query "Candida Albicans[Organism] AND latest[filter]" | \
-efetch -format docsum | \
+DATABASE=
+QUERY=
+FORMAT="docsum"
+FILE_STRING="_genomic.fna.gz"
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -db | --database )      shift
+                                DATABASE=$1
+                                ;;
+        -q | --query )          shift
+                                QUERY=$1
+                                ;;
+        -f | --format )          shift
+                                FORMAT=$1
+                                ;;
+        -fs | --file-string )   shift
+                                FILE_STRING=$1
+                                ;;
+        * )                     echo "Command not found"
+                                exit 1
+    esac
+    shift
+done
+
+wget `esearch -db "$DATABASE" -query "$QUERY" | \
+efetch -format "$FORMAT" | \
 xtract -pattern DocumentSummary -element FtpPath_GenBank | \
-awk -F"/" '{print $0"/"$NF"_genomic.fna.gz"}'`
+awk -F"/" '{print $0"/"$NF"'$FILE_STRING'"}'`
