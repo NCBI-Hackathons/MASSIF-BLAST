06fd15ceb7a1743e90b7f8466393e05a83faea21
Jessica Maia <Jessica_Maia@bd.com>
2018-03-14 17:05:07 -0400

    addiing the option of the output summary

--- src/py/parse_pilon_log.py
+++ src/py/parse_pilon_log.py
@@ -1,17 +1,23 @@
-import sys
 import os
 import re
 import subprocess
 import argparse
 
+# March 14, 2018
+# This Python script parses a Pilon standard output
+# This script is not yet finished
 
-#This Python script parses a Pilon standard output
 parser = argparse.ArgumentParser(add_help=True)
 parser.add_argument('--pilon_out', type=file, dest='pilon_out', required=True, help='Pilon standard out')
+parser.add_argument('--pilon_summary', type=file, dest='pilon_summary', required=True, help='Pilon Summary of standard out')
 args = parser.parse_args()
-print "Test arg parse", args.pilon_out
+
+# Open output file
+pilon_summary_file = open(args.pilon_summary, 'w+')
 
 for line in args.pilon_out.readlines():
-    line.strip('\n')
-    print line
+    if 'Scanning BAMs' in line:
+        pilon_summary_file.write(line)
+        print line
 
+# grep -w 'Scanning BAMs' -A 6 pilon_log
